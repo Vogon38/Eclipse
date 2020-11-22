8ddd3dc0de9f6f0430688a576f7fb14861b8b02e
Luana Cristini <luana@thecyborg.xyz>
2020-11-22 20:47:46 -0300

    Exerc_1

--- Estrutura de Dados/Exercício TAD/Exerc_1.java
+++ Estrutura de Dados/Exercício TAD/Exerc_1.java
@@ -11,15 +11,15 @@ public class Exerc_1 {
         
         cpf = JOptionPane.showInputDialog("Digite seu CPF neste formato (123.456.789-11): ");
           
-        d1 = cpf.substring(0, 1); n1 = Integer.parseInt(d1);
-        d2 = cpf.substring(1, 2); n2 = Integer.parseInt(d2);
-        d3 = cpf.substring(2, 3); n3 = Integer.parseInt(d3);
-        d4 = cpf.substring(4, 5); n4 = Integer.parseInt(d4);
-        d5 = cpf.substring(5, 6); n5 = Integer.parseInt(d5);
-        d6 = cpf.substring(6, 7); n6 = Integer.parseInt(d6);
-        d7 = cpf.substring(8, 9); n7 = Integer.parseInt(d7);
-        d8 = cpf.substring(9, 10); n8 = Integer.parseInt(d8);
-        d9 = cpf.substring(10, 11); n9 = Integer.parseInt(d9);
+        d1 = cpf.substring(0,1); n1 = Integer.parseInt(d1);
+        d2 = cpf.substring(1,2); n2 = Integer.parseInt(d2);
+        d3 = cpf.substring(2,3); n3 = Integer.parseInt(d3);
+        d4 = cpf.substring(4,5); n4 = Integer.parseInt(d4);
+        d5 = cpf.substring(5,6); n5 = Integer.parseInt(d5);
+        d6 = cpf.substring(6,7); n6 = Integer.parseInt(d6);
+        d7 = cpf.substring(8,9); n7 = Integer.parseInt(d7);
+        d8 = cpf.substring(9,10); n8 = Integer.parseInt(d8);
+        d9 = cpf.substring(10,11); n9 = Integer.parseInt(d9);
         
         verifica1 = n1 * 10 + n2 * 9 + n3 * 8 + n4 * 7 + n5 * 6 + n6 * 5 + n7 * 4 + n8 * 3 + n9 * 2;
         if (verifica1 % 11 < 2) {
