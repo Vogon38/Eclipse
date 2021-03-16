280302e264c06bfad0ce6d4fce4860aab0ff8527
Luana Cristini <luana@thecyborg.xyz>
2021-03-16 11:12:47 -0300

    :)

--- Programação Orientada a Objetos/Lista_03/src/Piramide.java
+++ Programação Orientada a Objetos/Lista_03/src/Piramide.java
@@ -1,48 +1,121 @@
+import java.util.Scanner;
 
 public class Piramide {
-	double altura_triangulo, ab_triangulo;
+		
+	public final int VOLUME_LATA = 18; //18 litros de tinta por lata
+	
+	double altura_triangulo, ab_triangulo, preco_tipo, preco_total;
+	double a1, litros, latas, volume;
 	int tipo_tinta;
 	
-	public Piramide(double altura_triangulo, double ab_triangulo, int tipo_tinta) {
-	}
-	
-	public static final int VOLUME_LATA = 18; //18 litros de tinta por lata
-	public static final double METROS_LATA = 85.68; //cada litro pinta 4,76 m²
-	
-	public void getPiramideValor() {
+	private void getCalculoPiramide() {
 		
-		double preco_tipo, preco_total;
-		double a1 = (this.altura_triangulo*this.altura_triangulo)+(this.ab_triangulo*this.ab_triangulo);
-		double litros = a1/4.76;
-		double latas = litros/VOLUME_LATA;
-		double volume = 1/3*(this.ab_triangulo*this.altura_triangulo);
+		Scanner entrada = new Scanner(System.in);
 		
+		System.out.println("Informe a altura do triângulo: ");
+		this.altura_triangulo = entrada.nextDouble();
+			
+		System.out.println("Informe a ab do triângulo: ");
+		this.ab_triangulo = entrada.nextDouble();
+			
+		System.out.println("Informe o tipo de tinta (1, 2 ou 3): ");
+		this.tipo_tinta = entrada.nextInt();
+			
+		this.a1 = (this.altura_triangulo*this.altura_triangulo)+(this.ab_triangulo*this.ab_triangulo);
+		this.litros = a1/4.76;
+		this.latas = this.litros/this.VOLUME_LATA;
+		this.volume = 1/3*(this.ab_triangulo*this.altura_triangulo);
+			
 		if (this.tipo_tinta == 1) {
-			preco_tipo = 127.90;	
+			this.preco_tipo = 127.90;	
 		} else if (this.tipo_tinta == 2) {
-			preco_tipo = 258.98;
+			this.preco_tipo = 258.98;
 		} else {
-			preco_tipo = 344.34;
+			this.preco_tipo = 344.34;
 		}
-		
+			
 		System.out.println("ab: " + this.ab_triangulo);
 		System.out.println("h: " + this.altura_triangulo);
-		System.out.println("a1: " + a1);
-		System.out.println("área da base: " + (a1*a1));
+		System.out.println("a1: " + this.a1);
+		System.out.println("área da base: " + (this.a1*this.a1));
 		System.out.println("tipo de tinta: " + this.tipo_tinta);
-		System.out.println("litros: " + litros);
-		System.out.println("latas: " + Math.round(latas));
-		System.out.println("preço: " + preco_tipo*latas);
-		System.out.println("volume: " + volume);
-	
-		public void setPiramide() {
-			
-	}
-		
-		public double getPiramide() {
-			
-		}
+		System.out.println("litros: " + this.litros);
+		System.out.println("latas: " + Math.round(this.latas));
+		System.out.println("preço: " + this.preco_tipo*this.latas);
+		System.out.println("volume: " + this.volume);
 		
 	}
+
+//	public double getAltura_triangulo() {
+//		return altura_triangulo;
+//	}
+//
+//	public void setAltura_triangulo(double altura_triangulo) {
+//		this.altura_triangulo = altura_triangulo;
+//	}
+//
+//	public double getAb_triangulo() {
+//		return ab_triangulo;
+//	}
+//
+//	public void setAb_triangulo(double ab_triangulo) {
+//		this.ab_triangulo = ab_triangulo;
+//	}
+//
+//	public double getPreco_tipo() {
+//		return preco_tipo;
+//	}
+//
+//	public void setPreco_tipo(double preco_tipo) {
+//		this.preco_tipo = preco_tipo;
+//	}
+//
+//	public double getPreco_total() {
+//		return preco_total;
+//	}
+//
+//	public void setPreco_total(double preco_total) {
+//		this.preco_total = preco_total;
+//	}
+//
+//	public double getA1() {
+//		return a1;
+//	}
+//
+//	public void setA1(double a1) {
+//		this.a1 = a1;
+//	}
+//
+//	public double getLitros() {
+//		return litros;
+//	}
+//
+//	public void setLitros(double litros) {
+//		this.litros = litros;
+//	}
+//
+//	public double getLatas() {
+//		return latas;
+//	}
+//
+//	public void setLatas(double latas) {
+//		this.latas = latas;
+//	}
+//
+//	public double getVolume() {
+//		return volume;
+//	}
+//
+//	public void setVolume(double volume) {
+//		this.volume = volume;
+//	}
+//
+//	public int getTipo_tinta() {
+//		return tipo_tinta;
+//	}
+//
+//	public void setTipo_tinta(int tipo_tinta) {
+//		this.tipo_tinta = tipo_tinta;
+//	}	
 	
 }
--- Programação Orientada a Objetos/Lista_03/src/Programa.java
+++ Programação Orientada a Objetos/Lista_03/src/Programa.java
@@ -1,26 +1,9 @@
-import java.util.Scanner;
-
 public class Programa {
 
-public static final int VOLUME_LATA = 18; //18 litros de tinta por lata
-public static final double METROS_LATA = 85.68; //cada litro pinta 4,76 m²
-
 	public static void main(String[] args) {
-		
-		double altura_triangulo, ab_triangulo;
-		int tipo_tinta;
-		
-		Scanner entrada = new Scanner(System.in);
-		
-		System.out.println("Informe a altura do triângulo: ");
-		altura_triangulo = entrada.nextDouble();
-		
-		System.out.println("Informe a ab do triângulo: ");
-		ab_triangulo = entrada.nextDouble();
-		
-		System.out.println("Informe o tipo de tinta (1, 2 ou 3): ");
-		tipo_tinta = entrada.nextInt();
+
+		Piramide classePiramide = new Piramide();
+		//classePiramide = getCalculoPiramide();
 		
 	}
-
 }
